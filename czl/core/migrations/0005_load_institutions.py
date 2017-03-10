fa857d3901ecdb3a7539098fa512514b967a88f6
Ionuț Ciocîrlan <ionut.ciocirlan@gmail.com>
2017-03-10 21:58:24 +0200

    institutions data, auto-loaded

---
+++ czl/core/migrations/0005_load_institutions.py
@@ -0,0 +1,42 @@
+from __future__ import unicode_literals
+
+import os.path
+import yaml
+from django.conf import settings
+from django.db import migrations
+
+
+SOURCE_FILE = os.path.join(settings.BASE_DIR, 'data', 'institutions.yaml')
+
+def forwards_func(apps, schema_editor):
+    Institution = apps.get_model("core", "Institution")
+    db_alias = schema_editor.connection.alias
+
+    with open(SOURCE_FILE, 'r') as f:
+        institutions = yaml.load(f)
+
+        Institution.objects.using(db_alias).bulk_create([
+            Institution(**inst) for inst in institutions
+        ])
+
+def reverse_func(apps, schema_editor):
+    Institution = apps.get_model("core", "Institution")
+    db_alias = schema_editor.connection.alias
+
+    with open(SOURCE_FILE, 'r') as f:
+        institutions = yaml.load(f)
+
+        Institution.objects.using(db_alias).filter(
+            id__in=[inst['id'] for inst in institutions]
+        ).delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0004_auto_20170309_2230'),
+    ]
+
+    operations = [
+        migrations.RunPython(forwards_func, reverse_func)
+    ]
